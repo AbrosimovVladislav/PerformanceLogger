1c64358714891f5bbb8d06d962fe60a50d2bdaee
vabrosim <Vladislav.Abrosimov@t-systems.com>
2020-11-21 20:30:12 +0300

    init

---
+++ src/main/java/ru/vakoom/performancelogger/PerformanceLoggerAutoConfiguration.java
@@ -0,0 +1,17 @@
+package ru.vakoom.performancelogger;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PerformanceLoggerAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public PerformanceLoggerAspect performanceLoggerAspect() {
+        return new PerformanceLoggerAspect();
+    }
+
+}
